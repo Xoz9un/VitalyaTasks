dae60322bfc326fa9d75cc3f2b8fdf1173502201
Vitaly Baranov <aleshapashkevicha@gmail.com>
2022-05-10 23:30:20 +0300

    add proj

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+         
+         string name = Console.ReadLine();
+         string surname = Console.ReadLine();
+            Console.WriteLine(name + " " + surname);
+
+        }
+    }
+}
