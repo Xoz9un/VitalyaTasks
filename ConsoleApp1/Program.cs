ec8491fac090e054d3fe8aabf9ccad02c879848d
Vitaly Baranov <aleshapashkevicha@gmail.com>
2022-05-10 23:36:51 +0300

    first Task

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,11 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-
+        
         static void Main(string[] args)
         {
-         
+            //Ввести две строки из консоли. Вывести их через пробел
+
          string name = Console.ReadLine();
          string surname = Console.ReadLine();
             Console.WriteLine(name + " " + surname);
